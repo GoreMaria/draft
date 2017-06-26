726209c5da2ff8da339004037e3e8e1f335d7fbe
GoreMaria <gorelkina_maria@mail.ru>
2017-06-26 21:44:04 +0300

    Delete keys

--- TakeBusDraft/TakeBusDraft/Services/ServiceKeys.cs
+++ TakeBusDraft/TakeBusDraft/Services/ServiceKeys.cs
@@ -8,8 +8,8 @@ namespace TakeBusDraft.Services
     public static class ServiceKeys
     {
         // qna knowledge base id 
-        public const string knowledgebaseId = "4073f0a1-6ddc-496d-82a4-1fe978036368";
+        public const string knowledgebaseId = "putknowledgebaseidhere";
         // qna subscription key 
-        public const string qnamakerSubscriptionKey = "0001d6aaadc24108bb75a17c8c5d6bc8";
+        public const string qnamakerSubscriptionKey = "putsubscriptionkeyhere";
     }
 }
